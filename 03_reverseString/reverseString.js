3d76427928f57f383c89021fa3ab08fc49512753
bigbossbry <brycmramirez@gmail.com>
2022-11-03 07:04:33 +0800

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wordReversed = "";
+    for(i = 0; i <= word.length; i++) {
+        wordReversed += word.charAt(word.length-i);
+    }
+    return wordReversed;
 };
 
 // Do not edit below this line
