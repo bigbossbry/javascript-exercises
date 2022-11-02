39156f72a16a0eafc71d7f4991ae55b5d9508003
bigbossbry <brycmramirez@gmail.com>
2022-11-03 06:41:46 +0800

    Finish exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(stringPara, numRepeat) {
+    if ( numRepeat < 0 ) {
+        string = "ERROR";
+    }
+    for (i = 0; i <= numRepeat; i++) {
+        if (i == 0){
+            string = "";
+        } else {
+            string += stringPara;
+        }
+    }
+    return string;
 };
 
 // Do not edit below this line
