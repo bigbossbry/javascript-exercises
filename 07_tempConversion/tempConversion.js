cc516e0d3a059d6eefd1265b9efe510f35967c50
bigbossbry <brycmramirez@gmail.com>
2022-11-09 19:18:29 +0800

    Finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
+//formulas C = (F-32)* (5/9)
+// F = (C * (9/5)) + 32
 
+const ftoc = function(f) {
+  let celsius;
+  celsius = (f-32)*(5/9);
+  return (Math.round(celsius*10)/10);
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let fahrenheit;
+  fahrenheit = ((c * 9/5) + 32);
+  return (Math.round(fahrenheit*10)/10);
 };
 
 // Do not edit below this line
